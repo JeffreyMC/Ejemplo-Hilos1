a19f6f681e8d2bf1f4bb4edb13a527f3044c666f
Jeffrey Muñoz C <jmunozcastro16@gmail.com>
2022-04-09 12:35:32 -0600

    Ejemplo finalizado

---
+++ Hilos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//intancio la clase Tareas 3 veces
+			// necesita dos parámetros: un String y un Int respectivamente
+			//el primero será el nombre del proceso y el segundo argumento, los segundos de duración
+			Tareas tarea1 = new Tareas("Hilo 1", 7);
+			Tareas tarea2 = new Tareas("Hilo 2", 10);
+			Tareas tarea3 = new Tareas("Hilo 3", 15);
+
+			//creo 3 hilos
+			// a cada uno le paso el mismo objeto 'tareas' pero con diferentes argumentos
+			// Note que no utilicé ParameterizedThread porque el proceso
+			//no requiere argumentos para iniciar
+			Thread hilo1 = new Thread(tarea1.Imprimir);
+			Thread hilo2 = new Thread(tarea2.Imprimir);
+			Thread hilo3 = new Thread(tarea3.Imprimir);
+
+
+			//inicio los hilos con Start
+			hilo1.Start();
+			hilo2.Start();
+			hilo3.Start();
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Hilos/Tareas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+	public class Tareas
+	{
+		//atributos de la clase
+		private string name;
+		private int second;
+
+		//constructor de la clase
+		public Tareas(string name, int second)
+		{
+			this.name = name;
+			this.second = second;
+		}
+
+		//esta es la función que ejecutará cada hilo
+		//primero el nombre y el tiempo que durará
+		//luego el hilo 'duerme' por el tiempo que se pasó por
+		//parámetro y se multiplica por 1000 (porque recuerde que se trabaja con
+		//milisegundos). Un segundo equivale a 1000 milisegundos
+		//por último se imprime el nombre del proceso y se indica que este terminó
+		public void Imprimir()
+		{
+			Console.WriteLine(name + " durará " + second + " segundos");
+			Thread.Sleep(second * 1000);
+			Console.WriteLine("\n---------------------------------------\n");
+			Console.WriteLine(name + " terminado");
+
+		}
+
+	}
+}
+
+
+//espero que le ayuden. Los hice lo más sencillos posibles
